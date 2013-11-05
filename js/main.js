76d738f136b0f86f86cf31043edd0b4ddf80662e
David Peterson <dbpete@yahoo.com>
2013-11-05 10:45:32 -0500

    Stuck on json read

--- js/main.js
+++ js/main.js
@@ -12,14 +12,14 @@ $('#mainPage').on('pageinit', function(){
 });	
 
 
-// addItem		
+// ADD ITEMS		
 $('#addItem').on('pageinit', function(){
 		//adding so key must be null	
 		$("#submit").on('click', storeData);
 });
     
     
-// ListItems		
+// LIST ITEMS		
 $('#listItems').on('pageinit', function(){
 
 	for(var i=0, len=localStorage.length; i<len; i++){
@@ -32,80 +32,113 @@ $('#listItems').on('pageinit', function(){
 	
 });
 
-$('#nextButton').click(function(){
-	maxCount = localStorage.length;
-	currentCount++;
-	if (currentCount > maxCount){currentCount=1
-		};
-	displayEditData();
-});
+// LIST ITEMS IN VARIOUS FORMATS
+$('#assignmentPage').on('pageinit', function(){
 
-$('#prevButton').click(function(){
-	maxCount = localStorage.length;	
-	currentCount--;
-	if (currentCount < 1){currentCount=localStorage.length};
-	displayEditData();
-});
-    
-    
-$('#editItemButton').click(function() {
-	maxCount = localStorage.length;
-	displayEditData();
-});
-
-
-  
-  
-function displayEditData(){
- 	//for(var i=0, len=localStorage.length; i<len; i++){
-		console.log('current count:' + currentCount + 'current key:' + currentKey);
-		currentKey = localStorage.key(currentCount-1);
-		
-		var value = localStorage.getItem(currentKey);
+    $('#jsonButton').click(function(){
+        alert('json clicked');
+        $.ajax({
+            url         : "xhr/data.json",
+            type        : "GET",
+            dataType    : "json",
+            success     : function(item, status) {
+                console.log(item, status)
+             //   var serializedItem = JSON.stringify(item);
+             //   console.log(serializedItem);
+            }
+        });
+    });
+/*	for(var i=0, len=localStorage.length; i<len; i++){
+		var key = localStorage.key(i);
+		var value = localStorage.getItem(key);
+	
 		var obj = JSON.parse(value);
-		
-		$('#editItemName').val(obj.itemName[1]);
-		$('#editDepartment').val(obj.department[1]);
-		$('#editDescription').val(obj.description[1]);
-		$('#editDesireDate').val(obj.desireDate[1]);
-		$('priority').val(obj.priority[1]);
-}
- 
-$('#updateButton').click(function(){
-    //Gather all form field values and store in object
-    //Object properties are going to contain an array with form label and input values
-
-    var id          = currentKey;
-    
-    var itemData        = {};
-        itemData.itemName       = ["Item Name", $('#editItemName').val()];
-        itemData.department     = ["Department", $('#editDepartment').val()];
-        itemData.description    = ["Description", $('#editDescription').val()];
-        itemData.desireDate     = ["DesiredDate", $('#editDesireDate').val()];
-        itemData.priority       = ["Search Tags", $('#editSearchTags').val()];
-	alert(itemData.department);
-    //Save Data into local storage: use stringify to convert our object to a string
-    localStorage.setItem(id, JSON.stringify(itemData));
-    alert("Item Updated!");
-}); 
+		$('#recentItemHeader').after('<li><a href="#editItems" id=key class="testClick"> Item: '+(obj.itemName[1])+ ' - ' + (obj.description[1])+ ' - Order by: '+ (obj.desireDate[1]) +  '</li>');
+	}
+*/	
+})
 
 
-$('#deleteButton').click(function(){
-    //Gather all form field values and store in object
-    //Object properties are going to contain an array with form label and input values
 
-    var id          = currentKey;
-    var ask = confirm("Are you sure you want to delete " + editItemName);
-    if(ask){
-		localStorage.removeItem(currentKey);
-		$('#nextButton').click();
-	}else{
-		alert("Item was not deleted.")
+// EDIT ITEMS		
+$('#editItems').on('pageinit', function(){
+
+    // NEXT BUTTON CLICKED
+	$('#nextButton').click(function(){
+		maxCount = localStorage.length;
+		currentCount++;
+		if (currentCount > maxCount){currentCount=1
+			};
+		displayEditData();
+	});
+
+    // PREVIOUS BUTTON CLICKED	
+	$('#prevButton').click(function(){
+		maxCount = localStorage.length;	
+		currentCount--;
+		if (currentCount < 1){currentCount=localStorage.length};
+		displayEditData();
+	});
+	    
+	// EDIT BUTTON CLICKED  
+	$('#editItemButton').click(function() {
+		maxCount = localStorage.length;
+		displayEditData();
+	});
+
+    // DISPLAY ITEM TO THE PAGE
+    function displayEditData(){
+     	//for(var i=0, len=localStorage.length; i<len; i++){
+    		console.log('current count:' + currentCount + 'current key:' + currentKey);
+    		currentKey = localStorage.key(currentCount-1);
+    		
+    		var value = localStorage.getItem(currentKey);
+    		var obj = JSON.parse(value);
+    		
+    		$('#editItemName').val(obj.itemName[1]);
+    		$('#editDepartment').val(obj.department[1]);
+    		$('#editDescription').val(obj.description[1]);
+    		$('#editDesireDate').val(obj.desireDate[1]);
+    		$('priority').val(obj.priority[1]);
     }
-
+    
+    // UPDATE THE ITEM WITH THE NEW INFORMATION 
+    $('#updateButton').click(function(){
+        //Gather all form field values and store in object
+        //Object properties are going to contain an array with form label and input values
+    
+        var id          = currentKey;
+        
+        var itemData        = {};
+            itemData.itemName       = ["Item Name", $('#editItemName').val()];
+            itemData.department     = ["Department", $('#editDepartment').val()];
+            itemData.description    = ["Description", $('#editDescription').val()];
+            itemData.desireDate     = ["DesiredDate", $('#editDesireDate').val()];
+            itemData.priority       = ["Search Tags", $('#editSearchTags').val()];
+    	alert(itemData.department);
+        //Save Data into local storage: use stringify to convert our object to a string
+        localStorage.setItem(id, JSON.stringify(itemData));
+        alert("Item Updated!");
+    }); 
+    
+    //DELETE THE ITEM
+    $('#deleteButton').click(function(){
+        //Gather all form field values and store in object
+        //Object properties are going to contain an array with form label and input values
+    
+        var id          = currentKey;
+        var ask = confirm("Are you sure you want to delete " + editItemName);
+        if(ask){
+    		localStorage.removeItem(currentKey);
+    		$('#nextButton').click();
+    	}else{
+    		alert("Item was not deleted.")
+        }
+    
+    });
 });
-
   
+// STORE DATA IN LOCAL STORAGE  
 function storeData(){
     //if no key then new item needing key
 /*    if(!key){
